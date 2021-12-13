c5be75056886e2c33d4b3ed05da9f4779c711bd6
Russell Wheeler <russellpeterwheeler@gmail.com>
2021-12-13 18:21:06 +0000

    - fixed carousel to allow images to be clicked to take them to game page

--- _includes/js/carousel.js
+++ _includes/js/carousel.js
@@ -4,13 +4,12 @@ let sliderCount = $("#slider").find(".slider-img li img").length;
 // Load images into the carousel
 let sliderImg = $("#slider").find(".slider-img");
 // Define the navigation arrows and pagination bullets.
-let sliderArrow = `<ul class="slider-arrow"><li class="arrow-left" role="button"><i class="fas fa-chevron-left"></i></li><li class="arrow-right" role="button"><i class="fas fa-chevron-right"></i></li></ul>`;
 let sliderDotLi = "";
 for (let i = 0; i < sliderCount; i++) {
     sliderDotLi += `<li><i class="fas fa-circle"></i></li>`;
 }
 let sliderDot = `<ul class="slider-dot">${sliderDotLi}</ul>`;
-$("#slider").append(sliderArrow + sliderDot);
+$("#slider").append(sliderDot);
 
 let activeDefaultCount = $(".slider-dot li.active").length;
 if (activeDefaultCount != 1) {
